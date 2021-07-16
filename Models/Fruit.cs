712e0430814be1f8e3dc63eb1cc38d058a930052
raphael.vidinha <raphael.vidinha@gmail.com>
2021-07-15 21:51:24 -0300

    Incrementando Models de Fruit, adicionando dependencias e gitignore

--- Models/Fruit.cs
+++ Models/Fruit.cs
@@ -8,6 +8,19 @@ namespace FruitStore.Models {
         [Required(ErrorMessage = "Este campo é obrigatório.")]
         [MaxLength(20, ErrorMessage = "Este campo deve conter entre 3 e 20 caracteres.")]
         [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 20 caracteres.")]
-        public string name { get; set; }
+        public string Name { get; set; }
+
+        [MaxLength(120, ErrorMessage = "Este campo deve conter entre 3 e 20 caracteres.")]
+        [MinLength(5, ErrorMessage = "Este campo deve conter entre 3 e 20 caracteres.")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        public string Image { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        public string StockQuantity { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        public decimal Price { get; set; }
     }
 }

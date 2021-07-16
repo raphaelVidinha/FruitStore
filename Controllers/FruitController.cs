651ed5bdb5b19b26e6973dc3bf4b079ddef96dab
raphael.vidinha <raphael.vidinha@gmail.com>
2021-07-15 23:25:11 -0300

    Finalizando operações de crud

--- Controllers/FruitController.cs
+++ Controllers/FruitController.cs
@@ -12,23 +12,86 @@ namespace FruitStore.Controllers
     [Route("v1/fruits")]
     public class FruitController : ControllerBase
     {
+        private readonly DataContext _context;
+        public FruitController(DataContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         [Route("")]
-        public async Task<ActionResult<List<Fruit>>> Get([FromServices] DataContext context){
-            return await context.Fruits.ToListAsync();
+        public async Task<ActionResult<List<Fruit>>> Get(){
+            return await _context.Fruits.ToListAsync();
         }
 
         [HttpPost]
         [Route("")]
-        public async Task<ActionResult<Fruit>> Post([FromServices] DataContext context, [FromBody] Fruit model){
+        public async Task<ActionResult<Fruit>> Post(Fruit model){
             if(!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
 
-            context.Fruits.Add(model);
-            await context.SaveChangesAsync();
+            _context.Fruits.Add(model);
+            await _context.SaveChangesAsync();
             return model;
+        }
+
+        [HttpGet]
+        [Route("id:int")]
+        public async Task<ActionResult<Fruit>> GetById(int id)
+        {
+            return await _context.Fruits.AsNoTracking().FirstOrDefaultAsync(x => x.Id.Equals(id));
+        }
+
+        private async Task<bool> FruitExists(int id) {
+            return await GetById(id) != null;
+        }
+
+        [HttpPut]
+        [Route("id:int")]
+        public async Task<ActionResult<Fruit>> PutTodoItem(int id, Fruit fruit)
+        {
+            if (id != fruit.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(fruit).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                if (await FruitExists(id) != true)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw ex;
+                }
+            }
+
+            return fruit;
+        }
+
+        [HttpDelete]
+        [Route("id:int")]
+        public async Task<ActionResult<bool>> Delete(int id)
+        {
+            var fruit = await _context.Fruits.FirstOrDefaultAsync(x => x.Id.Equals(id));
+
+            if(fruit == null){
+                return NotFound();
+            }
+
+            _context.Fruits.Remove(fruit);
+            await _context.SaveChangesAsync();
+
+            return true;
         }
     }
 }
